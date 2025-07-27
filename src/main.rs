4dbb43d057e5c4b6ede0f2b57e42698a21ec981b
Estanislao Crivos <estanislaocrivos@gmail.com>
2025-07-27 14:44:42 -0300

    Updates.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,13 @@
-#![no_std] // On embedded targets we do not include the std. library
+#![no_std]
 #![no_main]
 
 use cortex_m_rt::entry;
-use panic_halt as _; // Tells the compiler how to handle panic!
-use stm32f4xx_hal::{pac, prelude::*};
+use panic_halt as _;
+mod stm32f401re_blinky;
 
 #[entry] // Indicates where main() starts.
 fn main() -> ! {
-    // Minimal blinky example
-
-    let dp = pac::Peripherals::take().unwrap();
-    let rcc = dp.RCC.constrain();
-    let _clocks = rcc.cfgr.freeze();
-    let gpioa = dp.GPIOA.split();
-    let mut led = gpioa.pa5.into_push_pull_output();
-
-    loop {
-        led.set_high();
-        cortex_m::asm::delay(5_000_000);
-        led.set_low();
-        cortex_m::asm::delay(5_000_000);
-    }
+    // Uncomment next line for a minimal blinky example on STM32F401RE platform
+    stm32f401re_blinky::blinky();
+    // loop {}
 }
